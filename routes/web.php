4c672f2842ef8fec432205653efbb0870eb96df0
naykony <140612794+naykony@users.noreply.github.com>
2025-07-13 14:45:14 +0330

    add logout

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CretpostController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\CommentController;
+use Illuminate\Support\Facades\Auth;
 
 use App\Http\Con;
 
@@ -35,6 +36,7 @@ Route::post('/register', [AuthController::class, 'register'])->name('reg');
 
 
 Route::get('/prof', function () {return view('auth.prof');})->name('prof')->middleware('auth');
+Route::post('/logout', function () {Auth::logout();return redirect()->route('home');})->name('logout');
 
 
 Route::post('/post/{postId}/comment', [CommentController::class, 'store'])->name('comments.store');
